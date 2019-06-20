81aacb7b92542b5dec5572036d5088157bd4a6f6
Nate Kinne <nathan.kinne@gmail.com>
2019-06-20 12:56:32 -0500

    Weather Feature added

--- code.js
+++ code.js
@@ -1,6 +1,9 @@
 // basic term in comments
 // places: a resteraunt or park. a physical establishment
 // area: city 
+// ===============
+// google maps API
+// ===============
 // Sadly must be global because it is accessed by multiple functions and used by many google funcitons
 let map;
 // info window is the window that pops up when marker is clicked
@@ -52,10 +55,11 @@ function findPlace(results){
     // get the geocode for area to search in  
     let lat = results[0].geometry.location.lat();
     let lng = results[0].geometry.location.lng();
+    // get the weather
+    getWeather(lat, lng);
     // set search location to addr
     let place = new google.maps.LatLng(lat, lng);
     // center map on the search location
-    console.log(document.getElementById('map'))
     map = new google.maps.Map($('#map')[0], {
         center: place,
         zoom: 12
@@ -97,11 +101,11 @@ function createMarker(place) {
 // get results and display picture, name, link or place on map
 function parks(place){
     place.forEach(function(park){
+        // if the park has no picture data remove it
         if(!park.photos){
            console.log("No pictures");
            return 
         }
-       console.log(park.id);
         let picSrc = park.photos[0].getUrl();
         $('.results').append(`
         <div class="result" id="${park.id}">
@@ -127,6 +131,67 @@ function watchParks(park){
     });
 }
 
+// =================
+// Weather API
+// =================
+// initiate the weather API. get location and retrieve temp, clouds, UV index, heat index
+function getWeather(lat, lng){
+    // change when formatted
+    // &lat=38.123&lon=-78.543
+    fetch(`https://api.weatherbit.io/v2.0/current?&lat=${lat}&lon=${lng}&key=df10f63a96934282a5e65dde3bec6c8e`)
+        .then(response => response.json())
+        .then(responseJson => displayWeather(responseJson))
+        .catch(error => alert("Weather Api Error. Please try again later"));
+
+}
+
+// displays weather in the DOM
+function displayWeather(response){
+    console.log(response);
+    let data = response.data[0];
+    let temperature = convertTemp(data.temp);
+    // get sunscreen reccomendations
+    let suggestion = sunscreen(data.uv);
+    console.log(data.temp);
+    $('.temperature').text(`${temperature}`);
+    console.log(data.weather.description);
+    $('.weather-description').text(`${data.weather.description}`);
+    console.log(data.uv);
+    $('.uv-index').text(`${data.uv}`);
+    console.log(suggestion);
+    // sunscreen reccomendations
+    $('.sunscreen').text(`${suggestion}`);
+    console.log(temperature);
+}
+
+// convert the temperature to imperial
+function convertTemp(celsius){   
+    return (celsius * 9 / 5) + 32;;
+}
+
+// sunscreen reccomendations
+function sunscreen(uvData){
+    let suggestion = [
+        'It\'s night time you don\'t need sunscreen.',
+        'Probably best to wear a hat if you don\'t want a sun burn.',
+        'I would reccomend wearing wearing a hat and putting on sunscreen.',
+        'Be sure to put on plenty of sunscreen and wear a hat.',
+        'If you dont want to put on lots of sunscreen I would wait to take a walk until after 7p.m'
+    ];
+
+    if(uvData === 0){
+        return suggestion[0];
+    } else if (uvData <= 3  && uvData >0){
+        return suggestion[1]
+    }else if(uvData >=4 && uvData <= 6){
+        return suggestion[2]
+    }else if(uvData >= 5 && uvData <=7){
+        return suggestion[3];
+    }else if(uvData >=8){
+        return suggestion[4];
+    }
+}
+
 // watch the form for submit
 function watchForm(){
     $(document).on('submit', 'form', function (e) {
@@ -134,18 +199,23 @@ function watchForm(){
         e.preventDefault();
         // prepare the page for results
         prepPage();
+        // clear previous results
+        $('.result').remove();
         // get search term
         let term = $("#term").val();
         console.log(term);
         // displays map and initializes
         initialize(term);
-    })
+    });
 }
+
+
 // prepare page for search results
 // This will be animated in the future
 function prepPage(){
     $('.text').addClass('hidden');
     $('.results').removeClass('hidden');
+    $('.weather').removeClass('hidden');
 }
 
 (function(){
